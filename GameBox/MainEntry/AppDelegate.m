113dfabf902cb864368ffc9437d5c37c4d52d50d
Sing.Yi <23323231@qq.com>
2018-03-21 11:24:02 +0800

    20018-3-21 : 1st ->  create project and organize the directory

--- GameBox/MainEntry/AppDelegate.m
+++ GameBox/MainEntry/AppDelegate.m
@@ -17,6 +17,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+
+    self.window.rootViewController = [UIViewController new];
+    [self.window makeKeyAndVisible];
+
+    NSLog(@"interesting");
+
+
     return YES;
 }
 
