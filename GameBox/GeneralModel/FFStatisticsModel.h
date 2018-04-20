94601b80c0dfbce9c6a207256ef9745d2504e05a
Sing.Yi <23323231@qq.com>
2018-04-20 18:49:11 +0800

    2018-04-20 last commit add statistics

---
+++ GameBox/GeneralModel/FFStatisticsModel.h
@@ -0,0 +1,23 @@
+//
+//  FFStatisticsModel.h
+//  GameBox
+//
+//  Created by 燚 on 2018/4/20.
+//  Copyright © 2018年 Sans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFStatisticsModel : NSObject
+
+/** app install statistic */
++ (void)installStatistic;
+
+/** app start statistic */
++ (void)startStatistic;
+
+
+
+
+
+@end
