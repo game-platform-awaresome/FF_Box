75dbd1b98e9f4262d6ff87e09319c35bccc19f42
Sing.Yi <23323231@qq.com>
2018-08-20 16:50:14 +0800

    fix repair class

--- GameBox/GeneralClass/FFRepairClass/FFRepairViewController.m
+++ GameBox/GeneralClass/FFRepairClass/FFRepairViewController.m
@@ -9,12 +9,17 @@
 #import "FFRepairViewController.h"
 #import "FFDeviceInfo.h"
 
+#define ContentText @"      受苹果公司新版系统影响,部分应用存在闪退等无法正常使用的情况.敬请安装防闪退工具.\n      如果185sy游戏出现闪退,无法打开等情况,可用此工具一键修复,并可随时联系客服反馈问题,强烈建议您安装."
+
 @interface FFRepairViewController ()
 
-@property (nonatomic, strong) UIView    *backView;
-@property (nonatomic, strong) UIButton  *closeButton;
+@property (nonatomic, strong) UIView        *backView;
+@property (nonatomic, strong) UIButton      *closeButton;
+@property (nonatomic, strong) UIImageView   *logImageView;
+@property (nonatomic, strong) UILabel       *titleLabel;
+@property (nonatomic, strong) UILabel       *contentLabel;
 
-@property (nonatomic, strong) UIButton  *installButton;
+@property (nonatomic, strong) UIButton      *installButton;
 
 
 @end
@@ -42,6 +47,7 @@
     [super initUserInterface];
     self.view.backgroundColor = [UIColor colorWithWhite:0.1 alpha:0.3];
 
+
     self.backView = [UIView hyb_viewWithSuperView:self.view constraints:^(MASConstraintMaker *make) {
         make.size.mas_equalTo(CGSizeMake(self.view.bounds.size.width * 0.7, self.view.bounds.size.height * 0.6));
         make.center.mas_equalTo(CGPointZero);
@@ -50,27 +56,49 @@
     self.backView.layer.cornerRadius = 8;
     self.backView.layer.masksToBounds = YES;
 
-    self.closeButton = [UIButton hyb_buttonWithImage:@"" superView:self.view constraints:^(MASConstraintMaker *make) {
+    self.closeButton = [UIButton hyb_buttonWithImage:@"Game_repaire_close" superView:self.view constraints:^(MASConstraintMaker *make) {
         make.right.mas_equalTo(self.backView.mas_right);
         make.bottom.mas_equalTo(self.backView.mas_top);
         make.size.mas_equalTo(CGSizeMake(40, 40));
     } touchUp:^(UIButton *sender) {
         [self close];
     }];
-    self.closeButton.backgroundColor = kOrangeColor;
 
+    self.logImageView = [UIImageView hyb_imageViewWithImage:@"Game_repaire_logo" superView:self.backView constraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.backView).offset(30);
+        make.centerX.mas_equalTo(0);
+    }];
+
+    self.titleLabel = [UILabel hyb_labelWithText:@"防闪退修复工具" font:18 superView:self.backView constraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.logImageView.mas_bottom).offset(10);
+        make.left.mas_equalTo(self.backView).offset(8);
+        make.right.mas_equalTo(self.backView).offset(-8);
+        make.height.mas_equalTo(30);
+    }];
+    self.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
 
     self.installButton = [UIButton hyb_buttonWithTitle:@"立即安装" superView:self.backView constraints:^(MASConstraintMaker *make) {
         make.bottom.mas_equalTo(self.backView).offset(-30);
         make.left.mas_equalTo(self.backView).offset(40);
         make.right.mas_equalTo(self.backView).offset(-40);
-        make.height.mas_equalTo(44);
+        make.height.mas_equalTo(30);
     } touchUp:^(UIButton *sender) {
         [self respondsToLeftButton];
     }];
     self.installButton.backgroundColor = [FFColorManager blue_dark];
-    self.installButton.layer.cornerRadius = 22;
+    self.installButton.layer.cornerRadius = 15;
     self.installButton.layer.masksToBounds = YES;
+
+    self.contentLabel = [UILabel hyb_labelWithText:ContentText font:14 superView:self.backView constraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.titleLabel).offset(8);
+        make.left.mas_equalTo(self.backView).offset(8);
+        make.right.mas_equalTo(self.backView).offset(-8);
+        make.bottom.mas_equalTo(self.installButton.mas_top).offset(-9);
+    }];
+    self.contentLabel.numberOfLines = 0;
+    self.contentLabel.textColor = [FFColorManager textColorLight];
+    self.contentLabel.textAlignment = NSTextAlignmentJustified;
 }
 
 #pragma mark - method
